95185862bfd0bee4c333ffbaa1d081ec6d9f2986
zhaoweihan2015 <727095319@qq.com>
2017-03-11 11:10:50 +0800

    Revert "一次投票系统完成"
    This reverts commit 12e9c05512f1a0f9c68475d068326a19b9d0b7d9.

--- libs/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST','localhost');
-define('USER','root');
-define('PASSWORD','zwh19960426');
-define('DATABASE','votetable');
-?>
--- libs/config/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once ('config.php');
-if(!$mysql = mysqli_connect(HOST, USER, PASSWORD, DATABASE)){
-	echo mysqli_error();
-};
-$mysql->query('set header utf8');
-?>
